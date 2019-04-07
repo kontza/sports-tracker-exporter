c3caa5eaa270ddbd77b1ea7f22390730dd4413c6
Rumpu-Jussi <kontza@gmail.com>
2019-04-07 14:47:41 +0300

    Workouts are now saved to a file. If the file exists, workouts are downloaded from the server.

--- stdl.py
+++ stdl.py
@@ -2,6 +2,7 @@
 import argparse
 import datetime
 import getpass
+import json
 import logging
 import os
 import os.path
@@ -17,6 +18,7 @@ except:
 
 logging.basicConfig(format='%(asctime)-15s %(levelname)7s %(message)s', level=logging.INFO)
 logger = logging.getLogger(os.path.splitext(os.path.split(__file__)[-1])[0])
+workout_list = 'workouts.stdl'
 
 
 class VerboseAction(argparse.Action):
@@ -31,38 +33,50 @@ class VerboseAction(argparse.Action):
         logger.debug('Logging verbosely.')
 
 
-def get_list(args, session):
+def download_workout(args, session, workout_key):
+    'https://sports-tracker.com/apiserver/v1/workout/exportGpx/55db4fc1e4b094b778d42ed5?token=e78qo9jpe61f87p9d2baa1eq82qv60ui'
+    pass
+
+
+def get_list(args, session, output_filepath):
     url = 'https://sports-tracker.com/apiserver/v1/workouts'
+    logger.info('Loading workouts, this might take a while...')
     req = session.get(url, params={'limited': 'true', 'limit': 1000000})
     if req.status_code >= 200 and req.status_code < 300:
-        logger.info(req.json()['payload'][0])
+        logger.info('Workouts ({}) loaded...'.format(len(req.json()['payload'])))
+        with open(output_filepath, 'w') as output_file:
+            json.dump(req.json(), output_file, ensure_ascii=False, indent=2)
     else:
         logger.error('Failed to get the workout list: {} {}'.format(req.status_code, req.text))
 
 
 def run(args):
-    logger.info('Logging in to Sports Tracker.')
-    url = 'https://sports-tracker.com/apiserver/v1/login'
-    session = requests.Session()
-    req = session.post(url, params={'source': 'javascript'}, data={'l': args.user, 'p': args.password})
-    if req.status_code >= 200 and req.status_code < 300:
-        logger.info('Welcome {}.'.format(req.json()['realName']))
-        logger.debug("Response: {}".format(req.json()))
-        session.headers.update({'sttauthorization': req.json()['userKey']})
-        # session.cookies.update({
-        #     'sessionkey': req.json()['sessionkey'],
-        #     'sttauthorization': req.json()['userKey']})
-        get_list(args, session)
+    output_filepath = os.path.join(args.directory, workout_list)
+    if os.path.exists(output_filepath):
+        logger.info("Workouts list ({}) already exists, using it instead of downloading a new one.".format(output_filepath))
     else:
-        logger.error('Login failed: {} {}'.format(req.status_code, req.text))
+        logger.info('Logging in to Sports Tracker.')
+        url = 'https://sports-tracker.com/apiserver/v1/login'
+        password = getpass.getpass('Enter your Sports Tracker password: ')
+        session = requests.Session()
+        req = session.post(url, params={'source': 'javascript'}, data={'l': args.user, 'p': password})
+        if req.status_code >= 200 and req.status_code < 300:
+            logger.info('Welcome {}.'.format(req.json()['realName']))
+            logger.debug("Response: {}".format(req.json()))
+            session.headers.update({'sttauthorization': req.json()['userKey']})
+            get_list(args, session, output_filepath)
+        else:
+            logger.error('Login failed: {} {}'.format(req.status_code, req.text))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download excercises from Sports Tracker.')
     parser.add_argument('-v', '--verbose', help='increase output verbosity', action=VerboseAction)
-    parser.add_argument('-d', '--dir', default=os.getcwd(), help='the directory where to store exported files')
+    parser.add_argument('-d', '--directory', default=os.getcwd(), help='the directory where to store exported files')
     parser.add_argument('user', help='the user to log in as')
     args = parser.parse_args()
-    args.password = getpass.getpass('Enter your Sports Tracker password: ')
+    if not os.path.exists(args.directory):
+        logger.error("Output directory '{}' does not exist, cannot continue".format(args.directory))
+        exit()
     run(args)
     logger.info('All done, TTFN.')
