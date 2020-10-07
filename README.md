438077f695b8118af1a6bd47600acead9f63ddae
Rumpu-Jussi <kontza@gmail.com>
2020-10-07 19:32:00 +0300

    Docs updated. Formatting changes. Added a check for user name.

--- README.md
+++ README.md
@@ -5,10 +5,25 @@
     ```
 1. If you're using the Fish shell: before activating the virtualenv, check the deactivation function in  `./venv/bin/activate.fish` for the following. There exists an `if test -n "$_OLD_FISH_PROMPT_OVERRIDE"` -block. The first line of code inside that block must be `set -l fish_function_path`. Without that line, the script tries to clear the *fish_prompt*, and does not succeed in clearing the prompt, because fish apparently automatically loads *fish_prompt* from *$fish_function_path* if it exists.
 1. Now you can activate the virtualenv:
-    ```    
+    ```
     $ . venv/bin/activate.fish
     ```
+1. Download ChromeDriver: https://sites.google.com/a/chromium.org/chromedriver/home
+1. Make sure that your Chrome and ChromeDriver are the same version. ChromeDriver complains about it, if they don't match.
 
-# Running
+# Downloading From Sports Tracker
 On Mac/Linux: open a terminal, then
 `$ ./stdl.py -d output [Sports Tracker user ID]`
+
+# Uploading To Endomondo
+1. Create a file called `.env` in this directory.
+1. Add the following lines to it:
+    ```
+    ENDOMONDO_USERNAME=your.endomondo@account.com
+    ENDOMONDO_PASSWORD=y0urEnd0M0nd0Passw0rd
+    ```
+1. Have a directory ready containing FIT files to upload.
+1. Run the script:
+    ```
+    $ ./endomondo-uploader.py --directory ./fit-files-directory
+    ```
--- endomondo-uploader.py
+++ endomondo-uploader.py
@@ -15,41 +15,43 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
-chrome_driver = webdriver.Chrome()
-logging.basicConfig(format='%(asctime)-15s %(levelname)7s %(message)s', level=logging.INFO)
+chrome_driver = None
+logging.basicConfig(format="%(asctime)-15s %(levelname)7s %(message)s", level=logging.INFO)
 logger = logging.getLogger(os.path.splitext(os.path.split(__file__)[-1])[0])
 
 
 class VerboseAction(argparse.Action):
     def __init__(self, nargs=0, **kw):
         if nargs != 0:
-            raise ValueError('nargs for VerboseAction must be 0; it is just a flag.')
+            raise ValueError("nargs for VerboseAction must be 0; it is just a flag.")
         super().__init__(nargs=nargs, **kw)
 
     def __call__(self, parser, namespace, values, option_string=None):
         setattr(namespace, self.dest, True)
         logger.setLevel(logging.DEBUG)
-        logger.debug('Logging verbosely.')
+        logger.debug("Logging verbosely.")
 
 
 @atexit.register
 def driver_quit():
-    print('Closing the Chromedriver.')
-    chrome_driver.quit()
+    if chrome_driver:
+        print("Closing the Chromedriver.")
+        chrome_driver.quit()
 
 
 def login():
     try:
         chrome_driver.get("https://www.endomondo.com/login")
-        wait = WebDriverWait(chrome_driver, 10)
+        wait = WebDriverWait(chrome_driver, 1000)
         wait.until(lambda d: d.find_element_by_name("email"))
         inputElement = chrome_driver.find_element_by_name("email")
-        inputElement.send_keys(os.getenv('ENDOMONDO_USERNAME'))
+        logger.info(f"Logging in as {os.getenv('ENDOMONDO_USERNAME')}...")
+        inputElement.send_keys(os.getenv("ENDOMONDO_USERNAME"))
         inputElement = chrome_driver.find_element_by_name("password")
-        inputElement.send_keys(os.getenv('ENDOMONDO_PASSWORD'))
+        inputElement.send_keys(os.getenv("ENDOMONDO_PASSWORD"))
         inputElement.submit()
     except TimeoutException as e:
-        print('Login failed: {}'.format(e))
+        print("Login failed: {}".format(e))
         return False
     return True
 
@@ -59,22 +61,22 @@ def logout():
     hover = ActionChains(chrome_driver).move_to_element(avatar_element)
     hover.perform()
     try:
-        wait = WebDriverWait(chrome_driver, 10)
+        wait = WebDriverWait(chrome_driver, 1000)
         log_out = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@ng-click="logout()"]')))
         log_out.click()
     except TimeoutException:
-        print('Timed out while waiting for logout-button to be clickable.')
+        print("Timed out while waiting for logout-button to be clickable.")
 
 
 def upload(fit_file):
-    chrome_driver.get('https://www.endomondo.com/workouts/create')
+    chrome_driver.get("https://www.endomondo.com/workouts/create")
     try:
         # Handle import type selection.
         wait = WebDriverWait(chrome_driver, 10)
         # wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@id="ida"]'))).click()
         fileimport_div = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@class="fileImport"]')))
-        step_up = fileimport_div.find_element_by_xpath('..')
-        fileimport_btn = step_up.find_element_by_xpath('..')
+        step_up = fileimport_div.find_element_by_xpath("..")
+        fileimport_btn = step_up.find_element_by_xpath("..")
         fileimport_btn.click()
         # Handle the file selection iframe.
         WebDriverWait(chrome_driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, "iframed")))
@@ -85,9 +87,9 @@ def upload(fit_file):
         wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@name="reviewSumbit"]'))).click()
         wait.until(EC.url_matches("/workouts/latest$"))
         # Transfer done, rename file.
-        shutil.move(full_path, '{}.done'.format(full_path))
+        shutil.move(full_path, "{}.done".format(full_path))
     except TimeoutException:
-        print('Timed out while waiting to click file import buttons.')
+        print("Timed out while waiting to click file import buttons.")
         exit()
 
 
@@ -99,19 +101,19 @@ def run(args):
         wait = WebDriverWait(chrome_driver, 10)
         wait.until(lambda d: d.current_url == "https://www.endomondo.com/home")
     except TimeoutException:
-        print('Timed out on entering the landing page.')
+        print("Timed out on entering the landing page.")
         exit()
 
     # Close the premium ad.
     try:
         wait = WebDriverWait(chrome_driver, 10)
-        dismiss = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'MonthTrialPopup-close')))
+        dismiss = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "MonthTrialPopup-close")))
         dismiss.click()
     except TimeoutException:
-        print('No popup this time, good.')
+        print("No popup this time, good.")
 
     # Loop over fit-files.
-    for fit in sorted(glob.glob(os.path.join(args.directory,'*.fit'))):
+    for fit in sorted(glob.glob(os.path.join(args.directory, "*.fit"))):
         upload(fit)
         time.sleep(5)
 
@@ -119,16 +121,20 @@ def run(args):
     logout()
 
 
-if __name__ == '__main__':
-    load_dotenv()
-    parser = argparse.ArgumentParser(description='Upload excercises to Endomondo.')
-    parser.add_argument('-v', '--verbose', help='increase output verbosity', action=VerboseAction)
-    parser.add_argument('-d', '--directory', default=os.getcwd(), help='the directory where to load fit-files from')
-    parser.add_argument('-u', '--user', help='the user to log in as', default=os.getenv('ENDOMONDO_USERNAME'))
+if __name__ == "__main__":
+    load_dotenv(verbose=True)
+    parser = argparse.ArgumentParser(description="Upload excercises to Endomondo.")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action=VerboseAction)
+    parser.add_argument("-d", "--directory", default=os.getcwd(), help="the directory where to load fit-files from")
+    parser.add_argument("-u", "--user", help="the user to log in as", default=os.getenv("ENDOMONDO_USERNAME"))
     args = parser.parse_args()
     if not os.path.exists(args.directory):
-        logger.error("Output directory '{}' does not exist, cannot continue".format(args.directory))
+        logger.error(f"Output directory '{args.directory}' does not exist, cannot continue!")
         exit()
-    args.password = os.getenv('ENDOMONDO_PASSWORD')
+    if not os.getenv("ENDOMONDO_USERNAME"):
+        logger.error("Username not defined, cannot continue!")
+        exit()
+    args.password = os.getenv("ENDOMONDO_PASSWORD")
+    chrome_driver = webdriver.Chrome()
     run(args)
-    logger.info('All done, TTFN.')
+    logger.info("All done, TTFN.")
